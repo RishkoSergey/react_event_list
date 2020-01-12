1f374669962427e8de1ecc450ae2a03be2b9f18d
RishkoSergey <risko_sergey@mail.ru>
2020-01-12 04:25:19 +0300

    redux init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {
+        props.events.map(item => (
+          <li>
+            <b>{item.title}</b> - {item.price}
+          </li>
+        ))
+      }
+    </ul>
   );
 }
 
-export default App;
+const mapStateToProps = ({ events }) => ({
+  events: events.items
+});
+
+export default connect(mapStateToProps)(App);
